bf09407d5e62a2e3c545616c656e061f445297c9
hawwina18 <hawwina.18018@mhs.unesa.ac.id>
2021-02-16 23:34:51 +0700

    16feb2021

--- resources/views/livewire/about/about.blade.php
+++ resources/views/livewire/about/about.blade.php
@@ -1,3 +1,23 @@
 <div>
-    ini about yoi
+    <div class="container mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
+          <div class="max-h-96 md:h-screen">
+            <img class="w-screen h-screen object-cover object-top" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+          </div>
+          <div class="flex bg-gray-100 p-10">
+            <div class="mb-auto mt-auto max-w-lg">
+              <h1 class="text-3xl uppercase">Recehin</h1>
+              <p class="font-semibold mb-5">Open Chat Portal</p>
+              <p>Recehin adalah aplikasi chatting yang bersifat open chat bagi siapa saja. Recehin memiliki tujuan untuk memberantas kebosanan pengguna.</p>
+              <br>
+              <p><b> Kenapa harus "Recehin" ?</b></p>
+              <p>Karena Recehin merupakan aplikasi yang bisa menjadi sarana hiburan untuk pengguna. 
+                ketika anda merasa bosan, Anda bisa memulai percakapan random dan saling berbalas pesan melalui aplikasi recehin</p>
+              <br>
+              <p><b>Jadi Tunggu Apa Lagi ???</b></p>
+                <button class="bg-black rounded-md py-3 px-7 mt-6 text-white">START</button>
+            </div>
+          </div>
+        </div>
+      </div> 
 </div>
